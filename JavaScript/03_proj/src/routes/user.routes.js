793783e109d5071e7c379f96d3aa5b2627b08693
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-03-21 14:01:16 +0000

    add user register logic

--- JavaScript/03_proj/src/routes/user.routes.js
+++ JavaScript/03_proj/src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import {Router} from "express"
 import { registerUser } from "../controllers/user.controller.js"
+import { upload} from "../middleware/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount:1
+        },
+        { 
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser)
 // UserRouter.route("/login").post(login)
 
 export default router
