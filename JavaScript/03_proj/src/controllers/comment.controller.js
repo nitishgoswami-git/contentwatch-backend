d5b49029f615832fe3f6d8624c4ccb3bb4cdbfd4
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-04-02 14:19:09 +0000

    implement add, update, and delete functionalities for comments and playlists with validation

--- JavaScript/03_proj/src/controllers/comment.controller.js
+++ JavaScript/03_proj/src/controllers/comment.controller.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Like } from "../models/like.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
@@ -13,14 +14,130 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    //  get video_id
+    //  get content
+    //  verify video_id
+    //  verify content not empty
+    //  get user_id
+    //  create obj 
+    //  return res
+
+    const {video_id} = req.params
+    const {content} = req.body
+
+    if(!isValidObjectId(video_id)){
+        throw new ApiError(400,"Not a valid video_id")
+    }
+
+    if(!content){
+        throw new ApiError(400, "Content is required")
+    }
+    const comment = await Comment.create(
+        {
+            content,
+            video: video_id,
+            owner: req.user?._id
+        }
+    )
+    if(!comment){
+        throw new ApiError(400,"Something went wrong comment not added")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            comment,
+            "Comment added"
+        )
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    //  get comment id
+    //  get newcontent
+    //  verify comment id
+    //  find the comment document
+    //  verify owner == user
+    //  update the comment
+    //  return res
+
+    const {commentId} = req.params
+    const {newContent} = req.body
+
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Not a valid comment id")
+    }
+    
+    if(!newContent){
+        throw new ApiError(400, "Content cannot be empty")
+    }
+    const commentFile = await Comment.findById(commentId)
+    if(!commentFile){
+        throw new ApiError(400, "Comment not found")
+    } 
+    if(commentFile.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403, "You are not authorized to edit ")
+    }
+    const newComment = Comment.findByIdAndUpdate(commentId,{
+        $set:{
+            content: newContent
+        }
+    },{new:true})
+
+    if(!newComment){
+        throw new ApiError(400, "Something went wrong while updating")
+    }
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            newComment,
+            "Comment Updated"
+        )
+    )
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    //  get comment id
+    //  verify comment id
+    //  find comment document
+    //  verify if owner == user
+    //  delete comment and likes 
+    //  return res
+
+    const {commentId} = req.params
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400, "Not a valid Comment id")
+    }
+
+    const commentFile = await Comment.findById(commentId)
+    if(!commentFile){
+        throw new ApiError(400, "Comment not found")
+    }
+
+    if(commentFile.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403, "You are not the owner")
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId)
+    if(!deletedComment){
+        throw new ApiError(400,"Something went wrong while deleting")
+    }
+    await Like.deleteMany({
+        comment: commentId,
+    })
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "operation successfull"
+        )
+    )
 })
 
 export {
--- JavaScript/03_proj/src/controllers/playlist.controller.js
+++ JavaScript/03_proj/src/controllers/playlist.controller.js
@@ -6,9 +6,37 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
+    //TODO: create playlist
+    //  get title and desc
+    //  verify if empty
+    //  create playlist obj
+    //  return res
+
     const {name, description} = req.body
 
-    //TODO: create playlist
+    if(!(name||description)){
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const playlist = await Playlist.create(
+        {
+            name,
+            description,
+            owner : req.user?._id
+        }
+    )
+    if(!playlist){
+        throw new ApiError(400, "Something went wrong while creating playlist")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            playlist,
+            "Playlist created"
+        )
+    )
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
@@ -22,24 +50,174 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    //  get video_id
+    //  get playlist id
+    //  verfiy playlist and video ids
+    //  find playlist
+    //  find video
+    //  add video to playlist
+    //  return res
+
     const {playlistId, videoId} = req.params
+    if(!(isValidObjectId(playlistId)||isValidObjectId(videoId))){
+        throw new ApiError(400,"Invalid playlist or video id")
+    }
+    const playlist = await Playlist.findById(playlistId)
+    if(!playlist){
+        throw new ApiError(400,"Playlist not found")
+    }
+
+    if(playlist.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(400,"You are not authorized")
+    }
+    const addedVideoToPlaylist = await Playlist.findByIdAndUpdate(playlistId,{
+        $addToSet:{
+            videos: videoId
+        }
+    },{new: true})
+
+    if(!addVideoToPlaylist){
+        throw new ApiError(400, "Unable to add video to playlist")
+    }
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            addVideoToPlaylist,
+            "Video added to playlist"
+        )
+    )
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
     // TODO: remove video from playlist
+    //  get playlist_id and video_id
+    //  verify ids
+    //  find playlist 
+    //  verify if owner == user
+    //  remove video
+    //  return res
+    
+    const {playlistId, videoId} = req.params
+    if(!(isValidObjectId(playlistId) || isValidObjectId(videoId))){
+        throw new ApiError(400, "Not a valid Id")
+    }
 
+    const playlist = await Playlist.findById(playlistId)
+    if(!playlist){
+        throw new ApiError(400, "Playlist not found")
+    }
+
+    if(playlist.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(400, "You are not Authorized")
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(playlistId,{
+        $pull:{
+            videos:videoId
+        }
+    },{new:true})
+
+    if(!updatedPlaylist){
+        throw new ApiError(400, "Unable to delete videos")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            updatedPlaylist,
+            "Playlist updated"
+        )
+    )
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
     // TODO: delete playlist
+    //  get playlist Id 
+    //  verify id
+    //  find playlist
+    //  verify owner == req.user
+    //  delete
+    //  return res
+
+    const {playlistId} = req.params
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400, " Invalid id")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+    if(!playlist){
+        throw new ApiError(400, " Playlist not found")
+    }
+
+    if(playlist.owner.toString !== req.user?._id){
+        throw new ApiError(400,"You are not Authorized")
+    }
+
+    const deletedPlaylist = await Playlist.findByIdAndDelete(playlistId)
+    if(!deletedPlaylist){
+        throw new ApiError(400, " Unable to delete Playlist")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            deletedPlaylist,
+            "Playlist deleted"
+        )
+    )
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
+    //TODO: update playlist
+    //  get playlist id
+    //  name and description
+    //  verify playlist id
+    //  find playlist
+    //  verify owner == user
+    //  updated playlist
+    //  return res
+
     const {playlistId} = req.params
     const {name, description} = req.body
-    //TODO: update playlist
+
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400, "Not a valid Id")
+    }
+
+    if (!(name || description)){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+    if(!playlist){
+        throw new ApiError(400, "Playlist not found")
+    }
+
+    if(playlist.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(400,"You are not Authorized")
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(playlistId,{
+        $set:{
+            name,
+            description
+        }
+    },{new : true})
+
+    if(!updatedPlaylist){
+        throw new ApiError(400, "Unable to update Playlist")
+    }
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            updatedPlaylist,
+            "Playlist updated"
+        )
+    )
 })
 
 export {
