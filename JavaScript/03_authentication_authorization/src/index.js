32dc45afe51f55f224deb54417ead5ca5d168aae
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-04-11 05:38:03 +0000

    add jwt - add controllers

--- JavaScript/03_authentication_authorization/src/index.js
+++ JavaScript/03_authentication_authorization/src/index.js
@@ -1,6 +1,18 @@
 import dotenv from "dotenv"
 import {app} from "./app.js"
+import connectDB from "./db/index.js"
+import { connection } from "mongoose"
 
 dotenv.config({
     path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running at port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB Connection Failed",err)
 })
